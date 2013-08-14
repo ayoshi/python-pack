6bec70bdc283073bfeed285ab94fd259266db11d
Alexander Goldberg <algold@gmail.com>
2013-08-14 23:49:58 +0300

    Add epy

--- init.el
+++ init.el
@@ -1,20 +1,9 @@
-;;(live-add-pack-lib "ipython")
+(live-add-pack-lib "emacs-for-python")
 
-;; (autoload 'dash-at-point "dash-at-point"
-;; 	            "Search the word at point with Dash." t nil)
+;; Fix for some incompatibility shit
+(defun yas/initialize nil)
 
-;;ipython
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args "--no-color"
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(require 'epy-init)
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
