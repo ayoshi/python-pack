9b39c384c51236b293a2427451a8a23a4f26cbae
Alexander Goldberg <alexg@infinidat.com>
2013-12-06 11:52:12 +0200

    Add support for pyenv

--- init.el
+++ init.el
@@ -1,5 +1,18 @@
 (install-packs '(elpy))
 
+;; Support for pyenv
+(defvar pyenv-path "~/.pyenv"
+  "pyenv root directory.")
+
+(when (file-exists-p pyenv-path)
+  (let ((bin-path (concat pyenv-path "/bin"))
+        (shims-path (concat pyenv-path "/shims")))
+    (setenv "PATH"
+            (concat bin-path ":" shims-path ":"
+                    (getenv "PATH")))
+    (setq python-check-command (concat shims-path "/flake8"))
+    (setq elpy-rpc-python-command (concat shims-path "/python"))))
+
 (elpy-enable)
 (elpy-use-ipython)
 
